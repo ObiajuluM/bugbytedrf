82ab5d8079794ceb8c9187475db9ac4c1c969685
ObiajuluM <oambanefo@gmail.com>
2025-06-24 20:11:16 +0100

    - Second video - Implement ProductSerializer with fields: id, name, description, price, stock - Add price validation in ProductSerializer to ensure price is greater than 0 - Define product_list view to return a list of all products - Define product_detail view to return a specific product by primary key - Update urls.py to include paths for product_list and product_detail views - Add settings.json for GitHub Copilot commit message generation instructions

--- api/serializers.py
+++ api/serializers.py
@@ -3,8 +3,24 @@ from .models import Product, Order, OrderItem
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         # define the model it's going to serialize
         model = Product
-        # tuple with all the fields it's going to convert to json
-        fields = ()
+        # tuple with all the fields it's going to convert to serialize (json)
+        fields = (
+            "id",
+            "name",
+            "description",
+            "price",
+            "stock",
+        )
+
+    # field level validation function
+    # validate_<field_name>
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Price must be greater than 0",
+            )
+        return value
--- api/views.py
+++ api/views.py
@@ -1,1 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+# function based views
+@api_view(["GET"])
+def product_list(request: Request):
+    # queryset
+    products = Product.objects.all()
+    # serializer
+    serializer = ProductSerializer(
+        products,
+        # to return a list of items
+        many=True,
+    )
+    # return a json response containing the serialized data
+    return Response(
+        serializer.data,
+    )
+
+
+@api_view(["GET"])
+def product_detail(request: Request, pk: int):
+    # queryset to get object with primary key or 404
+    product = get_object_or_404(Product, pk=pk)
+    # serializer
+    serializer = ProductSerializer(product)
+    # return a json response containing the serialized data
+    return Response(serializer.data)
